2deb12206ded11893177a47a0d617a96020452a6
Mateusz Szymoński <mateusz.szymonski@gmail.com>
2022-11-11 20:22:11 +0100

    Init

---
+++ Assets/Mighty/Core/MightyAudioManager.cs
@@ -0,0 +1,156 @@
+﻿/* 
+NAME:
+    Mighty Audio Manager
+
+DESCRIPTION:
+
+
+USAGE:
+    Look under the code below 
+
+TODO:
+   There is bug when playing random sound multiple are being played (one by one?)
+*/
+
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using NaughtyAttributes;
+
+namespace Mighty
+{
+    public enum SoundType
+    {
+        Effect,
+        Music
+    };
+
+    [System.Serializable]
+    public class MightySound
+    {
+        public string name;
+        public AudioClip clip;
+
+        [Range(0f, 1f)]
+        public float volume = 1.0f;
+        [Range(0.1f, 3f)]
+        public float pitch = 1.0f;
+
+        public bool playOnAwake;
+        public bool loop;
+
+
+        public SoundType soundType;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+
+    public class MightyAudioManager : MonoBehaviour
+    {
+        private static MightyAudioManager instance;
+        public static MightyAudioManager Instance { get { return instance; } }
+
+        [ReorderableList] public MightySound[] sounds;
+        public AudioMixerGroup musicMixerGroup;
+        public AudioMixerGroup effectsMixerGroup;
+
+        void Awake()
+        {
+            instance = this;
+
+            if (musicMixerGroup == null || effectsMixerGroup == null)
+            {
+                Debug.LogError("[MightyAudioManager] Sound mixer groups not set");
+            }
+
+            foreach (MightySound sound in sounds)
+            {
+                sound.source = gameObject.AddComponent<AudioSource>();
+                sound.source.clip = sound.clip;
+                sound.source.volume = sound.volume;
+                sound.source.pitch = sound.pitch;
+                sound.source.loop = sound.loop;
+                sound.source.playOnAwake = sound.playOnAwake;
+
+                if (sound.soundType == SoundType.Effect)
+                {
+                    sound.source.outputAudioMixerGroup = effectsMixerGroup;
+                }
+                if (sound.soundType == SoundType.Music)
+                {
+                    sound.source.outputAudioMixerGroup = musicMixerGroup;
+                }
+                if (sound.playOnAwake)
+                {
+                    sound.source.Play();
+                }
+            }
+        }
+
+        public void PlaySound(string soundName)
+        {
+            MightySound sound = Array.Find(sounds, soundFind => soundFind.name == soundName);
+            if (sound == null)
+            {
+                Debug.LogWarning("[MightyAudioManager] Sound: " + soundName + " not found!");
+                return;
+            }
+            sound.source.Play();
+        }
+
+        public void StopSound(string soundName)
+        {
+            MightySound sound = Array.Find(sounds, soundFind => soundFind.name == soundName);
+            if (sound == null)
+            {
+                Debug.LogWarning("[MightyAudioManager] Sound: " + soundName + " not found!");
+                return;
+            }
+            sound.source.Stop();
+        }
+
+        public bool IsSoundPlaying(string soundName)
+        {
+            MightySound sound = Array.Find(sounds, soundFind => soundFind.name == soundName);
+            if (sound == null)
+            {
+                Debug.LogWarning("[MightyAudioManager] Sound: " + soundName + " not found!");
+                return false;
+            }
+            return sound.source.isPlaying;
+        }
+
+        public void PlayRandomSound(params string[] soundNames)
+        {
+            string soundName = soundNames[UnityEngine.Random.Range(0, soundNames.Length)];
+            MightySound sound = Array.Find(sounds, soundFind => soundFind.name == soundName);
+            if (sound == null)
+            {
+                Debug.LogWarning("[MightyAudioManager] Randomized sound: " + soundName + " not found!");
+                return;
+            }
+            sound.source.Play();
+        }
+
+        public void SetMusicMixerVolume(float sliderValue)
+        {
+            musicMixerGroup.audioMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        }
+
+        public void SetEffectsMixerVolume(float sliderValue)
+        {
+            effectsMixerGroup.audioMixer.SetFloat("EffectsVolume", Mathf.Log10(sliderValue) * 20);
+        }
+    }
+}
+
+/* 
+USAGE:
+    Add this component to GameObject in Hierarchy
+    Add sounds to it in inspector
+    audioManager.PlaySound("SoundName"); // Play sound named "SoundName" in inspector
+    audioManager.StopSound("SoundName"); // Stop playing sound named "SoundName" in inspector
+    audioManager.PlayRandomSound("Sound1", "Sound2", "Sound3",) // Randomly choose and play one of sounds passed as parameter
+    
+*/
---
+++ Assets/Mighty/Utilities/FlashJuicer.cs
@@ -0,0 +1,113 @@
+﻿/* 
+NAME:
+    Flash Juicer
+
+DESCRIPTION:
+    Warning: Animated parameters should NOT be changed in playing mode by other scripts or components
+
+USAGE:
+    Add this as component to the object you want to animate
+    GetComponent<FlashJuicer>().StartJuicing();
+    GetComponent<FlashJuicer>().StopJuicing();
+
+TODO:
+   
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+namespace Mighty
+{
+    [ExecuteInEditMode]
+    public class FlashJuicer : MonoBehaviour
+    {
+        public Material flashMaterial;
+        public float flashDuration;
+
+        Material startMaterial;
+        Renderer rend;
+        bool active;
+        float flashTimer;
+
+        [Header("Collisions")]
+        public bool detectCollisions;
+        [ShowIf("detectCollisions")] [Tooltip("Object will detect collisions with objects with these tags")] public string[] tagsToCollide;
+
+        void Start()
+        {
+            rend = GetComponent<Renderer>();
+            startMaterial = rend.sharedMaterial;
+        }
+
+        void Update()
+        {
+            Evaluate();
+        }
+
+        public void StartJuicing()
+        {
+            flashTimer = 0;
+            if (!active)
+            {
+                rend.sharedMaterial = flashMaterial;
+            }
+            active = true;
+        }
+
+        void Evaluate()
+        {
+            if (active)
+            {
+                if (flashTimer < flashDuration)
+                {
+                    flashTimer += Time.deltaTime;
+                }
+                else
+                {
+                    rend.sharedMaterial = startMaterial;
+                    active = false;            
+                }
+            }
+        }
+
+        public void StopJuicing()
+        {
+            active = false;
+            rend.sharedMaterial = startMaterial;
+        }
+
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            if (detectCollisions)
+            {
+                for (int i = 0; i < tagsToCollide.Length; i++)
+                {
+                    if (collision.transform.tag == tagsToCollide[i])
+                    {
+                        StartJuicing();
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (detectCollisions)
+            {
+                for (int i = 0; i < tagsToCollide.Length; i++)
+                {
+                    if (collision.transform.tag == tagsToCollide[i])
+                    {
+                        StartJuicing();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
