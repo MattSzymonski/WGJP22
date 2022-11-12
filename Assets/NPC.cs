4a9d4e1942959932d866b8d1098675e0f5512a1d
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2022-11-12 14:29:28 +0100

    Add option for NPC posession and anim transition.

--- Assets/NPC.cs
+++ Assets/NPC.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    public bool isPosessed = false;
     private bool collided = false;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isPosessed)
+        {
+            animator.SetBool(0, true);  // isIDLE
+        }
         
     }
 
