f684105cbc4fa29ec4405c41fb75220af4399c36
Mateusz Szymoński <mateusz.szymonski@gmail.com>
2022-11-13 07:30:22 +0100

    sounds

--- Assets/Scripts/ValueStorage.cs
+++ Assets/Scripts/ValueStorage.cs
@@ -26,7 +26,7 @@ public class ValueStorage : MonoBehaviour
 
     public void TriggerShake()
     {
-        Camera.main.transform.parent.GetComponent<CameraShaker>().ShakeOnce(2.4f, 1f, 1f, 1.25f);
+        Camera.main.transform.parent.GetComponent<CameraShaker>().ShakeOnce(2.0f, 0.7f, 1f, 1.55f);
     }
 
 }
